c56bec36b9bfe7f40c8758944036d5534b041340
Samuel Ballard-Adams <s.ballard-adams@outlook.com>
2019-03-13 07:20:38 +0000

    Adding skeletal basic string counter.
    Thinking about this, it might be worth refactoring this to use a generic solution using IComparable in some way.
    I'm concerned about the second requirement, for comparing the number though, so I'll stick with what I know will work first.

---
+++ TechnicalSkillsDemo/TechnicalSkillsDemo/BasicStringCounter.cs
@@ -0,0 +1,26 @@
+﻿// <copyright file="BasicStringCounter.cs" company="Samuel Ballard-Adams">
+// Copyright (c) Samuel Ballard-Adams. All rights reserved.
+// Licensed under the GNU General Public License v3 license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace TechnicalSkillsDemo
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using TechnicalSkillsDemo.Interfaces;
+
+    /// <summary>
+    /// A basic counter for finding the count of a given pattern in a set of strings.
+    /// </summary>
+    public class BasicStringCounter : IStringCounter
+    {
+        /// <inheritdoc/>
+        public int FindPatternCount(string pattern, params string[] subjects)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
