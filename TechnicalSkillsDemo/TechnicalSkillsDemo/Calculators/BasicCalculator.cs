2629bb569ffdc55ed382ebc1c0f4a1b5d5adc82a
Samuel Ballard-Adams <s.ballard-adams@outlook.com>
2019-03-13 06:59:32 +0000

    Adding skeletal BasicCalculator.
    I always find it's best to add the class first, that way the merge deltas are a bit easier to read. :)

---
+++ TechnicalSkillsDemo/TechnicalSkillsDemo/Calculators/BasicCalculator.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="BasicCalculator.cs" company="Samuel Ballard-Adams">
+// Copyright (c) Samuel Ballard-Adams. All rights reserved.
+// Licensed under the GNU General Public License v3 license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace TechnicalSkillsDemo
+{
+    using System;
+    using TechnicalSkillsDemo.Interfaces;
+
+    /// <summary>
+    /// A basic calculator with no tricks.
+    /// </summary>
+    public class BasicCalculator : ICalculator
+    {
+        /// <inheritdoc/>
+        public int Sum(params int[] numbers)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
