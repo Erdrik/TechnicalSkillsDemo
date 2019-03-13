904a1232ddc7ff0e6b84f1d9aaef77d807217d7b
Samuel Ballard-Adams <s.ballard-adams@outlook.com>
2019-03-13 07:29:59 +0000

    Added failing unit tests.
    Ordinarily I probably wouldn't commit a failing unit test, depending on what kind of continuous integration we had running.
    It depends on the workflow, but if it would spam my teammates with bad emails, I'd make sure they were green first.
    
    I'm trying to show my workflow here. :)

--- TechnicalSkillsDemo/TechnicalSkillsDemoTests/StringCounterTests.cs
+++ TechnicalSkillsDemo/TechnicalSkillsDemoTests/StringCounterTests.cs
@@ -6,18 +6,24 @@
 namespace TechnicalSkillsDemoTests
 {
     using NUnit.Framework;
+    using TechnicalSkillsDemo;
 
     public class StringCounterTests
     {
+        private const string textTest = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eanean sodales justo et Enim ornare, a congue lacus commodo.";
+        private const string numberTest = "17272838119191929838299111";
+
         [SetUp]
         public void Setup()
         {
         }
 
-        [Test]
-        public void Test1()
+        [TestCase(12, textTest, TestName = "Requirement: Lorem ipsum has 12 \"e\"s.")]
+        [TestCase(8, textTest, TestName = "Requirement: 17272838119191929838299111 has 8 \"1\"s.")]
+        public void PatternsWillBeCounted(int expectedCount, params string[] text)
         {
-            Assert.Pass();
+            var stringCounter = new BasicStringCounter();
+            Assert.DoesNotThrow(() => Assert.AreEqual(expectedCount, text));
         }
     }
 }
